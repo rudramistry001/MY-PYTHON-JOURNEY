881f854a4f79db9b526a097a49800e800d51d943
RUDRA MISTRY <rudramistry74@yahoo.com>
2024-07-13 20:40:12 +0530

    pr-2,3,4

---
+++ 03_io_statements.py
@@ -0,0 +1,6 @@
+#This is a simple Calculator that used arithmetic operators to perform all the calculations For two digits
+
+a = int(input('Write the first Number?\n'))
+b = int(input('Write the second Number?\n'))
+print('The value of First Number is ',a)
+print('The value of Second Number is',b)
---
+++ 04_if_else.py
@@ -0,0 +1,10 @@
+#This is a python program to check if you are a coder..
+
+a = str(input('Are you a coder???\n'))
+
+if(a == "yes"):{
+    print("YEs you are a coder...")
+}
+else:{
+        print("Np you are not a coder....")
+    }
