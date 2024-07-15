45f9e69f04ecbe4e522a9bf227d5fd1191aa1cc0
RUDRA MISTRY <rudramistry74@yahoo.com>
2024-07-15 21:41:43 +0530

    05_ numeric dattatpes

---
+++ 05 _ DATATYPES/05_numeric_datatypes.py
@@ -0,0 +1,48 @@
+print("Data type specifies the type of value a variable requires to do various operations without causing an error. By default, python provides the following built-in data types:")
+
+print("The python Datatypes can be mainly divided in to 8 categories :-")
+
+print("1. Numeric Datatype:-")
+print("The numeric dataype consists of three datatypes :-")
+
+print("a) Int :-")
+print("example :-")
+a: int  = -2
+print(a)
+a: int  = 0
+print(a)
+a: int  = 21
+print(a)
+
+print("a) Float :-")
+print("example :-")
+a: float  = -2.01
+print(a)
+a: int  = 0.002
+print(a)
+a: int  = 21.000345
+print(a)
+
+
+
+
+print("b) Float :-")
+print("example :-")
+a: float  = -2.01
+print(a)
+a: int  = 0.002
+print(a)
+a: int  = 21.000345
+print(a)
+
+print("c) Complex :-")
+print("example :-")
+a: complex  = 6 + 2j
+print(a)
+a: complex = -3 + 2j
+print(a)
+a: int  = 0 - 4j
+print(a)
+
+
+
